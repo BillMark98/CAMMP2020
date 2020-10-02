4897c763914ce055e3066207826b6188a073309a
BillMark98 <hupanweibill@gmail.com>
2020-10-02 19:03:09 +0200

    add divideCurve 2 three region algorithms

--- MyNotes.md
+++ MyNotes.md
@@ -28,15 +28,18 @@ because in `python3`, `range` will return an iterator
 np.random.choice(range(lowerBound, upperBound), num , replace= False)
 ```
 
-* [run python script](https://www.knowledgehut.com/blog/programming/run-python-scripts) or [here](https://realpython.com/run-python-scripts/#:~:text=To%20run%20Python%20scripts%20with,python3%20hello.py%20Hello%20World!)
-Note that in python shell, using 
-
-
-will cause the problem `python name __file__ is not defined` ,because the syntax:
+* [run python script](https://www.knowledgehut.com/blog/programming/run-python-scripts) or [here](https://realpython.com/run-python-scripts/#:~:text=To%20run%20Python%20scripts%20with,python3%20hello.py%20Hello%20World!) or [here](https://stackoverflow.com/questions/17247471/how-to-run-a-python-script-from-idle-interactive-shell)
+Note that in python shell, using will cause the problem `python name __file__ is not defined` ,because the syntax:
 
 ```python
 if __name__ == (__file__):
     do_something
+```
+
+* [test float equal](https://www.quora.com/How-does-one-correctly-compare-two-floats-in-Python-to-test-if-they-are-equal)
+
+```python
+abs(a-b) < epsilon
 ```
 
 ## fileIO
@@ -62,12 +65,37 @@ basically, it's because if it is `N * 1` or `1 * N` only one variable needs to b
 * to create a one row dataframe, need also to put each element as array, like
 
 ```python
-
+df = pd.DataFrame({
+    "myName":["one"],
+    "myVal": [1]
+})
 ```
+
+doing this withoug an array, will generate an error message `ValueError: If using all scalar values, you must must pass an index`
+
+* [convert dictionary to dataframe](https://stackoverflow.com/questions/18837262/convert-python-dict-into-a-dataframe) or [documentation](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.from_dict.html)
+
+* [multiple plots in different figures](https://www.kite.com/python/answers/how-to-plot-multiple-graphs-on-a-single-figure-using-pandas-in-python)
 
 ## numpy
 
 * [numpy.select](https://numpy.org/doc/stable/reference/generated/numpy.select.html)
+
+## matplotlib
+
+* [x labels overlapping](https://stackoverflow.com/questions/26700598/matplotlib-showing-x-tick-labels-overlapping)
+
+* [too many indices for array ](https://stackoverflow.com/questions/49809027/matplotlib-subplots-too-many-indices-for-array)
+analogous problem in plotting in `pandas`, if the axes is one-dimensional, just use one dimension for the axes argument
+
+* [global legends](https://stackoverflow.com/questions/7526625/matplotlib-global-legend-and-title-aside-subplots)
+
+```python
+    fig, axs = plt.subplots()
+    fig.suptitle("Global title")
+``` 
+
+* [set legends for several lines](https://stackoverflow.com/questions/33322349/set-legend-for-plot-with-several-lines-in-python)
 
 ## re
 
@@ -79,3 +107,11 @@ m = r.match("foobar231")
 m.group(1) # 'foobar'
 m.group(2) # '231'
 ```
+
+## statistics
+
+* [pandas normality test](https://machinelearningmastery.com/a-gentle-introduction-to-normality-tests-in-python/), [ways to test normality](https://towardsdatascience.com/6-ways-to-test-for-a-normal-distribution-which-one-to-use-9dcf47d8fa93)
+
+## jupyter
+
+* use `stdin` to import file written in `vim`
--- statisticsAnalysis.py
+++ statisticsAnalysis.py
@@ -3,10 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import shapiro
 from scipy.stats import chisquare
+from sklearn.linear_model import LinearRegression
 import fileIO
 
 DEFAULT_NUM = 10000
 EPSILON = 1e-20
+INFINITIY = 1000000
+NEG_INFINITY = -1000000
 def almostEqual(a,b,epsilon = EPSILON):
     return np.abs(a-b) < epsilon
 
@@ -118,12 +121,11 @@ def dfNormalityTest(df, num = DEFAULT_NUM, lowPercentile = 0.02, upPercentile = 
     ### To do
     ### add concentration, molecule, info
     timeStep = df["samplingrate"].unique()
-    dataFrameCol = ["concentration", "samplingrate", "molecule", "lowPercentile","upPercentile"] + normalityTestCol
+    dataFrameCol = ["concentration", "samplingrate", "molecule", "lowPercentile","upPercentile", "alpha"] + normalityTestCol
     testResultFinalDf = pd.DataFrame(columns = dataFrameCol)
     concentrationLists = df["concentration"].unique()
     for samplingRate in timeStep:
         df_samplingLevel = df[df["samplingrate"] == samplingRate]
-        "df_samp"
         for concentration in concentrationLists:
             moleculeConcentrationDict = fileIO.getMoleculeConentration(concentration)
             columnConcentration = ["time"] + normalityTestCol + ["concentration", "molecule"]
@@ -175,6 +177,7 @@ def dfNormalityTest(df, num = DEFAULT_NUM, lowPercentile = 0.02, upPercentile = 
                     "molecule": [molecule],
                     "lowPercentile": [lowPercentile],
                     "upPercentile":[upPercentile],
+                    "alpha": [alpha]
                 }
                 )                    
                 for key, value in testResultDict.items():
@@ -204,7 +207,7 @@ def dfTest(df, num = DEFAULT_NUM, lowPercentile = 0.02, upPercentile = 0.1, alph
     ### To do
     ### add concentration, molecule, info
     timeStep = df["samplingrate"].unique()
-    dataFrameCol = ["concentration", "samplingrate", "molecule", "lowPercentile","upPercentile"] + normalityTestCol
+    dataFrameCol = ["concentration", "samplingrate", "molecule", "lowPercentile","upPercentile","alpha", "distribution"] + normalityTestCol
     testResultFinalDf = pd.DataFrame(columns = dataFrameCol)
     concentrationLists = df["concentration"].unique()
     for samplingRate in timeStep:
@@ -261,6 +264,8 @@ def dfTest(df, num = DEFAULT_NUM, lowPercentile = 0.02, upPercentile = 0.1, alph
                     "molecule": [molecule],
                     "lowPercentile": [lowPercentile],
                     "upPercentile":[upPercentile],
+                    "alpha": [alpha],
+                    "distribution": [distribution]
                 }
                 )                    
                 for key, value in testResultDict.items():
@@ -269,7 +274,7 @@ def dfTest(df, num = DEFAULT_NUM, lowPercentile = 0.02, upPercentile = 0.1, alph
                 testResultFinalDf = testResultFinalDf.append(df_testResult)
     return testResultFinalDf         
 
-def histPlot(df, timePoints, num, bins = 50, normalityTestCol = ["MSD_xy"]):
+def histPlot(df, timePoints, timeNum , particleNum, bins = 50, normalityTestCol = ["MSD_xy"]):
     """
     plot the histogram of df
 
@@ -280,32 +285,50 @@ def histPlot(df, timePoints, num, bins = 50, normalityTestCol = ["MSD_xy"]):
     timePoints: array-like structure
     the time points from which to choose points to plot
 
-    num: int
+    timeNum: int
     number of time points to choose
+
+    particleNum: int
+    number of particles to choose from
 
     """
     
     ## make sure the timePoints are unique
-    timePoints = timePoints.unique()
+    timePoints = list(set(timePoints))
     concentrationLists = df["concentration"].unique()
     
+    # select certain particles
+    # calculate the total timeNum of trajectories
+    numTrajectories = len(df["trajectory"].unique())    
+    numTrajectories = min(numTrajectories, particleNum)
+    trajectoryIndex = chooseIndex(df["trajectory"],particleNum)
+    trajectories = np.array(df["trajectory"])[trajectoryIndex]
+    # subset the df
+    df = df[df["trajectory"].isin(trajectories)]
 
     columns = ["time"] + normalityTestCol
     df_prune = df[columns]    
     timePointsNum = len(timePoints)
 
-    num = makeDivisible(num)
-    numberOfPoints = min(timePointsNum, num)
-    timeLists = np.random.choice(timePoints, size = numberOfPoints, replace = False)
+    timeNum = makeDivisible(timeNum)
+    numberOfPoints = min(timePointsNum, timeNum)
+    if (numberOfPoints > timePointsNum):
+        timeLists = np.random.choice(timePoints, size = numberOfPoints, replace = False)
+    else:
+        timeLists = timePoints
+
     timeLists.sort()
     # df_prune = df_prune[df_prune[almostEqual(df_prune["time"], timeLists)]]
     ## to do 
     ## adapt plotting 
     ncol = 2
-    nrow = int(num / ncol)
+    nrow = int(timeNum / ncol)
+
+
 
     fig,axes = plt.subplots(nrow,ncol)
     fig.tight_layout(pad = 5.0)
+    fig.suptitle("total number of particles chosen: " + str(numTrajectories), fontsize = 20)
     count = 1
     for elem in normalityTestCol:
         for time in timeLists:
@@ -316,3 +339,220 @@ def histPlot(df, timePoints, num, bins = 50, normalityTestCol = ["MSD_xy"]):
             plt.xlabel(elem)
             ## figSub.autofmt_xdate()
             count += 1            
+
+    plt.show()
+
+def diffArr(arr):
+    """
+
+    calculates the difference between each elements of arr
+
+    """
+# not sure if some arr starts indexing with 0
+# for example, doing a slicing of a dataframe, will cause arr starts at index 1
+# df_prune = df[1:]
+# diffArr(df_prune["MSD_xy"])  <---- index starts at 1
+    lenArr = len(arr)
+    arTemp = [None] * (lenArr - 1)
+    count = 0
+    for val in arr:
+        if (count == 0):
+            oldval = val
+        else:
+            arTemp[count - 1] = val - oldval
+            oldval = val
+        count += 1
+    return np.array(arTemp)
+
+def eliminateZeroTime(df):
+    """
+    eliminate the 0 time row, (doing log will cause problem)
+
+    -----
+    To do
+    ------
+    figure out how to drop 0 that emerge at rows other than the first line
+    """
+
+    if (almostEqual(df.iloc[0,:]["time"], 0)):
+        return df[1:]
+    else:
+        return df
+
+def findLowUpIndex(arr, lowBound = NEG_INFINITY, upBound = INFINITIY, startIndex = -1):
+    """
+    find the left, right index of an array, that has a continuous range lying between [lowBound, upBound]
+    only count the first time this range is hit, and with consideration to the startIndex
+
+    -------
+    Parameters:
+    -------
+    arr: array like structure
+
+    lowBound: float
+
+    upBound: float
+
+    startIndex: int
+
+    ------
+    To do:
+    ------
+    optimise the code for lowBound == NEG_INFINITY, upBOUND == INFINITY
+    """
+    
+    # make sure it is a numpy.array
+    arr = np.array(arr)
+    boolArr = (arr < upBound) & (arr > lowBound)
+    leftIndex = -1
+    rightIndex = -1
+    leftFound = False
+    rightFound = False
+    continuous = True
+    for i in range(len(boolArr)):
+        if (i >= startIndex):
+            if ((boolArr[i] == True) and (leftFound == False)):
+                leftFound = True
+                leftIndex = i
+            elif (leftFound == True):
+                # need to first check if the leftIndex is already found
+                if((boolArr[i]) and (not rightFound) and (continuous)):
+                    rightIndex = i
+                elif (rightFound == False):
+                    # boolArr[i] == False
+                    rightFound = True
+                    continuous = False
+                    break
+    print("lowBound: " + str(lowBound))
+    print("upBound: " + str(upBound))
+    print("leftIndex: " + str(leftIndex))
+    print("rightIndex: " + str(rightIndex))
+    return leftIndex, rightIndex
+
+def divide3Region(df, columns = ["MSD_xy_mean"],threshold2_low = 0.3,threshold2_high = 0.6 ):
+    """
+
+    very specific for MSD analysis, given the msd, divide the series into three regions
+    assume df has "time" as one of its column name
+    ------
+    Parameters
+    -------
+
+    return: three lists and the slopes
+    example usage l1,l2,l3,slopes = divide3Region(df, columns = ["MSD_xy"])
+    df_1 = df.iloc[l1]
+
+    ------
+    To do
+    ------
+
+    make several columns possible
+
+    """
+    threshold1down = 1.6
+    threshold3_low = 0.8
+    if (threshold3_low < threshold2_high + EPSILON):
+        threshold3_low += 0.09
+    threshold3_high = 1.2
+    df_prune = eliminateZeroTime(df)
+    if (len(columns) > 1):
+        raise Exception("currenlty only division works for one column")
+    
+    for col in columns:
+        # timeSeries = df_prune["time",col]
+        tLog = np.log(df_prune["time"])
+        msdLog = np.log(df_prune[col])
+        tLogDiff = diffArr(tLog)
+        msdLogDiff = diffArr(msdLog)
+        slopes = msdLogDiff / tLogDiff
+        ## find the first region
+        l1Dummy, r1 = findLowUpIndex(slopes, lowBound= threshold1down)
+        l2,r2 = findLowUpIndex(slopes, lowBound=threshold2_low, upBound= threshold2_high)
+        # have to make sure that l3 starts to count after the region 2
+        l3,r3Dummy = findLowUpIndex(slopes, lowBound= threshold3_low, upBound = threshold3_high, startIndex=r2)
+        list1 = list(range(0,r1 + 1))
+        list2 = list(range(l2,r2 + 1))
+        list3 = list(range(l3,len(slopes)))
+        return list1, list2, list3, slopes
+
+def simpleLinearRegression(xArr,yArr):
+    """
+    do a linear regression s.t yArr_approx = k * xArr + b
+
+    ----
+    Parameters:
+    ----
+
+    return: k,b
+    """
+    model = LinearRegression().fit(xArr,yArr)
+    return model.coef_, model.intercept_
+
+def msdRegression(df, columns = ["MSD_xy_mean"]):
+    """
+    do a linear regression s.t log(columns) = k * log(time) + b
+
+    ----
+    Pay Attention:
+    ----
+    Assume df has no zero Time
+
+    -----
+    Parmaeters:
+    -----
+
+    return: k, b
+
+    ----
+    To do:
+    -----
+
+    Making multiple columns possible
+
+    """
+    tLog = np.log(df["time"])
+
+    for col in columns:
+        msdLog = np.log(df[col])
+        return simpleLinearRegression(tLog, msdLog)
+
+def msdFittingPlot(df, columns = ["MSD_xy_mean"]):
+    """
+    plot the msd and the curve fitting
+
+    ----
+    To do:
+    ----
+    df should eliminate 0?
+    making multiple columns possible
+    """
+    df = eliminateZeroTime(df)
+
+    for col in columns:
+        list1, list2, list3, slopes = divide3Region(df,columns = [col])
+        indexLists = [list1,list2,list3]
+        dfLists = [df.iloc[indexLists[var]] for var in range(3)]
+        fig, axes = plt.subplots()
+        # plot the whole msd
+        legends = ["MSD","ballistic","subdiffusion","brownian"]
+        tLog = np.log(df["time"])
+        msdLog = np.log(df[col])
+        plt.plot(tLog,msdLog, label = legends[0])
+        
+        # for i in range(3):
+        #     k,b = msdRegression(dfLists[i], columns = [col])
+        #     ts = np.log(dfLists[i])
+        #     ys = k * ts + b
+        #     plt.plot(ts, ys, label = legends[i + 1])
+        
+        plt.show()
+
+
+if __name__ == "__main__":
+    arr = np.array([2,3,4,5,6,8])
+    arrT = diffArr(arr)
+    print(arrT)
+
+    charArr = ["hi","there","yo","yo1","haha"]
+    tempIndx = chooseIndex(charArr,2, 0,0.5)
+    print(tempIndx)
